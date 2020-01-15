2baa87ee78f18e108c4b8e5612f9e9802bad2bdd
Naftal Timotheus <tnaffh@gmail.com>
2020-01-15 17:13:35 +0200

    start

---
+++ src/Providers/RouteeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tnaffh\Routee\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RouteeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
